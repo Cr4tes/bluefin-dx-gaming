f3df62fc6f9bb9a451f62d19d7bcf810d35b57ee
rcraita <rcraita@yahoo.com>
2025-07-23 23:09:36 +0300

    add bazzite packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,230 @@
 
 set -ouex pipefail
 
-### Install packages
+# Install patched fwupd
+# Install Valve's patched Mesa, Pipewire, Bluez, and Xwayland
+# Install patched switcheroo control with proper discrete GPU support
+RUN --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=bind,from=ctx,source=/,target=/ctx \
+    --mount=type=tmpfs,dst=/tmp \
+    declare -A toswap=( \
+        ["copr:copr.fedorainfracloud.org:bazzite-org:bazzite"]="wireplumber" \
+        ["copr:copr.fedorainfracloud.org:bazzite-org:bazzite-multilib"]="pipewire bluez xorg-x11-server-Xwayland" \
+        ["terra-extras"]="switcheroo-control" \
+        ["terra-mesa"]="mesa-filesystem" \
+        ["copr:copr.fedorainfracloud.org:ublue-os:staging"]="fwupd" \
+    ) && \
+    for repo in "${!toswap[@]}"; do \
+        for package in ${toswap[$repo]}; do dnf5 -y swap --repo=$repo $package $package; done; \
+    done && unset -v toswap repo package && \
+    dnf5 versionlock add \
+        pipewire \
+        pipewire-alsa \
+        pipewire-gstreamer \
+        pipewire-jack-audio-connection-kit \
+        pipewire-jack-audio-connection-kit-libs \
+        pipewire-libs \
+        pipewire-plugin-libcamera \
+        pipewire-pulseaudio \
+        pipewire-utils \
+        wireplumber \
+        wireplumber-libs \
+        bluez \
+        bluez-cups \
+        bluez-libs \
+        bluez-obexd \
+        xorg-x11-server-Xwayland \
+        switcheroo-control \
+        mesa-dri-drivers \
+        mesa-filesystem \
+        mesa-libEGL \
+        mesa-libGL \
+        mesa-libgbm \
+        mesa-va-drivers \
+        mesa-vulkan-drivers \
+        fwupd \
+        fwupd-plugin-flashrom \
+        fwupd-plugin-modem-manager \
+        fwupd-plugin-uefi-capsule-data && \
+    dnf5 -y install \
+        mesa-va-drivers.i686 && \
+    dnf5 -y install --enable-repo="*rpmfusion*" --disable-repo="*fedora-multimedia*" \
+        libaacs \
+        libbdplus \
+        libbluray \
+        libbluray-utils && \
+    /ctx/cleanup
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# Remove unneeded packages
+RUN --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=bind,from=ctx,source=/,target=/ctx \
+    --mount=type=tmpfs,dst=/tmp \
+    dnf5 -y remove \
+        ublue-os-update-services \
+        firefox \
+        firefox-langpacks \
+        htop && \
+    /ctx/cleanup
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Install new packages
+RUN --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=bind,from=ctx,source=/,target=/ctx \
+    --mount=type=tmpfs,dst=/tmp \
+    dnf5 -y install \
+        https://github.com/bazzite-org/cicpoffs/releases/download/master/cicpoffs.rpm && \
+    dnf5 -y install \
+        bazaar \
+        libdex-0.9.1 \
+        iwd \
+        twitter-twemoji-fonts \
+        google-noto-sans-cjk-fonts \
+        lato-fonts \
+        fira-code-fonts \
+        nerd-fonts \
+        sunshine \
+        python3-pip \
+        libadwaita \
+        duperemove \
+        cpulimit \
+        sqlite \
+        xwininfo \
+        xrandr \
+        compsize \
+        ryzenadj \
+        ddcutil \
+        input-remapper \
+        libinput-utils \
+        i2c-tools \
+        lm_sensors \
+        fw-ectool \
+        fw-fanctrl \
+        udica \
+        ladspa-caps-plugins \
+        ladspa-noise-suppression-for-voice \
+        pipewire-module-filter-chain-sofa \
+        python3-icoextract \
+        tailscale \
+        webapp-manager \
+        btop \
+        duf \
+        fish \
+        lshw \
+        xdotool \
+        wmctrl \
+        libcec \
+        yad \
+        f3 \
+        pulseaudio-utils \
+        lzip \
+        p7zip \
+        p7zip-plugins \
+        rar \
+        libxcrypt-compat \
+        vulkan-tools \
+        extest.i686 \
+        xwiimote-ng \
+        fastfetch \
+        glow \
+        gum \
+        vim \
+        cockpit-networkmanager \
+        cockpit-podman \
+        cockpit-selinux \
+        cockpit-system \
+        cockpit-navigator \
+        cockpit-storaged \
+        topgrade \
+        ydotool \
+        stress-ng \
+        snapper \
+        btrfs-assistant \
+        edk2-ovmf \
+        qemu \
+        libvirt \
+        lsb_release \
+        uupd \
+        ds-inhibit \
+        rocm-hip \
+        rocm-opencl \
+        rocm-clinfo \
+        waydroid \
+        cage \
+        wlr-randr && \
+    curl -Lo /tmp/ls-iommu.tar.gz $(curl https://api.github.com/repos/HikariKnight/ls-iommu/releases/latest | jq -r '.assets[] | select(.name| test(".*x86_64.tar.gz$")).browser_download_url') && \
+    mkdir -p /tmp/ls-iommu && \
+    sed -i 's|uupd|& --disable-module-distrobox|' /usr/lib/systemd/system/uupd.service && \
+    setcap 'cap_sys_admin+p' /usr/bin/sunshine-v* && \
+    dnf5 -y --setopt=install_weak_deps=False install \
+        rocm-hip \
+        rocm-opencl \
+        rocm-clinfo \
+        rocm-smi && \
+    mkdir -p /etc/xdg/autostart && \
+    sed -i~ -E 's/=.\$\(command -v (nft|ip6?tables-legacy).*/=/g' /usr/lib/waydroid/data/scripts/waydroid-net.sh && \
+    sed -i 's/ --xdg-runtime=\\"${XDG_RUNTIME_DIR}\\"//g' /usr/bin/btrfs-assistant-launcher && \
+    curl -Lo /usr/bin/installcab https://raw.githubusercontent.com/bazzite-org/steam-proton-mf-wmv/master/installcab.py && \
+    chmod +x /usr/bin/installcab && \
+    curl -Lo /usr/bin/install-mf-wmv https://raw.githubusercontent.com/bazzite-org/steam-proton-mf-wmv/refs/heads/master/install-mf-wmv.sh && \
+    chmod +x /usr/bin/install-mf-wmv && \
+    tar --no-same-owner --no-same-permissions --no-overwrite-dir -xvzf /tmp/ls-iommu.tar.gz -C /tmp/ls-iommu && \
+    rm -f /tmp/ls-iommu.tar.gz && \
+    cp -r /tmp/ls-iommu/ls-iommu /usr/bin/ && \
+    curl -Lo /tmp/scopebuddy.tar.gz https://github.com/HikariKnight/ScopeBuddy/archive/refs/tags/$(curl https://api.github.com/repos/HikariKnight/scopebuddy/releases/latest | jq -r '.tag_name').tar.gz && \
+    mkdir -p /tmp/scopebuddy && \
+    tar --no-same-owner --no-same-permissions --no-overwrite-dir -xvzf /tmp/scopebuddy.tar.gz -C /tmp/scopebuddy && \
+    rm -f /tmp/scopebuddy.tar.gz && \
+    cp -r /tmp/scopebuddy/ScopeBuddy-*/bin/* /usr/bin/ && \
+    /ctx/cleanup
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# Install Steam & Lutris, plus supporting packages
+# Downgrade ibus to fix an issue with the Steam keyboard
+RUN --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=bind,from=ctx,source=/,target=/ctx \
+    --mount=type=tmpfs,dst=/tmp \
+    dnf5 -y swap \
+    --repo copr:copr.fedorainfracloud.org:bazzite-org:bazzite \
+        ibus ibus && \
+    dnf5 versionlock add \
+        ibus && \
+    dnf5 -y install \
+        gamescope.x86_64 \
+        gamescope-libs.x86_64 \
+        gamescope-libs.i686 \
+        gamescope-shaders \
+        jupiter-sd-mounting-btrfs \
+        umu-launcher \
+        dbus-x11 \
+        xdg-user-dirs \
+        gobject-introspection \
+        libFAudio.x86_64 \
+        libFAudio.i686 \
+        latencyflex-vulkan-layer \
+        vkBasalt.x86_64 \
+        vkBasalt.i686 \
+        mangohud.x86_64 \
+        mangohud.i686 \
+        libobs_vkcapture.x86_64 \
+        libobs_glcapture.x86_64 \
+        libobs_vkcapture.i686 \
+        libobs_glcapture.i686 \
+        VK_hdr_layer && \
+    dnf5 -y --setopt=install_weak_deps=False install \
+        steam \
+        lutris && \
+    dnf5 -y remove \
+        gamemode && \
+    curl -Lo /tmp/latencyflex.tar.xz $(curl https://api.github.com/repos/ishitatsuyuki/LatencyFleX/releases/latest | jq -r '.assets[] | select(.name| test(".*.tar.xz$")).browser_download_url') && \
+    mkdir -p /tmp/latencyflex && \
+    tar --no-same-owner --no-same-permissions --no-overwrite-dir --strip-components 1 -xvf /tmp/latencyflex.tar.xz -C /tmp/latencyflex && \
+    rm -f /tmp/latencyflex.tar.xz && \
+    mkdir -p /usr/lib64/latencyflex && \
+    cp -r /tmp/latencyflex/wine/usr/lib/wine/* /usr/lib64/latencyflex/ && \
+    curl -Lo /usr/bin/latencyflex https://raw.githubusercontent.com/bazzite-org/LatencyFleX-Installer/main/install.sh && \
+    chmod +x /usr/bin/latencyflex && \
+    curl -Lo /usr/bin/winetricks https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks && \
+    chmod +x /usr/bin/winetricks && \
+    /ctx/cleanup
