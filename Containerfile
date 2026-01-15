e81368bec8d1aefe6c9a9b8a2b096e6199220440
Radu Craita <rcraita@yahoo.com>
2026-01-15 16:58:04 +0200

    clean /var/run symlink
    clean /var/run symlink

--- Containerfile
+++ Containerfile
@@ -23,8 +23,10 @@ RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
     --mount=type=tmpfs,dst=/tmp \
-    /ctx/build.sh && \
-    ostree container commit
+    /ctx/build.sh
+
+### Fix /var/run symlink to point to /run
+RUN rm -rf /var/run && ln -s /run /var/run
     
 ### LINTING
 ## Verify final image and contents are correct.
--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,7 @@ mkdir -p /var/roothome
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y --allowerasing lutris \
+dnf5 install -y lutris \
     steam
 
 # compile custom schemas
@@ -27,4 +27,4 @@ glib-compile-schemas /usr/share/glib-2.0/schemas
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
